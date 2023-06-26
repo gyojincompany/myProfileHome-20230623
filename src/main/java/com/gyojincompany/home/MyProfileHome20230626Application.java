c4118fc803b75734193237636df036719b81f203
gyojincompany <luckyred@gmail.com>
2023-06-26 10:26:50 +0900

    기본환경설정 완료

---
+++ src/main/java/com/gyojincompany/home/MyProfileHome20230626Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProfileHome20230626Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProfileHome20230626Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/home/MyProfileHome20230626ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProfileHome20230626ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
